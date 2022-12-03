225f06aff473da6c78a0c9818ba0b1efa1fd4ba8
haizhoulee <134524345+haizhoulee@users.noreply.github.com>
2022-12-03 17:22:45 +0000

    \ufeff ChangeLog: update

--- benches/cp.rs
+++ benches/cp.rs
@@ -2,4 +2,16 @@
 
 extern crate test;
 extern crate rand;
-extern crate nda
+extern crate ndarray;
+extern crate pcg_rand;
+extern crate random_world;
+
+use test::{Bencher, black_box};
+use pcg_rand::Pcg32;
+use rand::{Rng, SeedableRng};
+use ndarray::prelude::*;
+
+use random_world::cp::*;
+use random_world::ncm::*;
+
+fn gener

7ee482c667e6a144178e4f1a0d221e60756a7dde
haizhoulee <134524345+haizhoulee@users.noreply.github.com>
2022-12-03 17:29:35 +0000

    documentation updates.

--- benches/cp.rs
+++ benches/cp.rs
@@ -14,4 +14,9 @@ use ndarray::prelude::*;
 use random_world::cp::*;
 use random_world::ncm::*;
 
-fn gener
+fn generate_data(n: usize, d: usize, n_labels: usize, seed: [u64; 2])
+        -> (Array2<f64>, Array1<usize>) {
+    let mut rng = Pcg32::from_seed(seed);
+
+    let inputs = Array::from_iter(rng.gen_iter::<f64>()
+                                     .take(n

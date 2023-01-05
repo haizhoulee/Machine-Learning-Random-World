ffdbbe9aefeac08b659c12924a3b10e90b81c78e
haizhoulee <134524345+haizhoulee@users.noreply.github.com>
2023-01-05 22:34:00 +0000

    [RUN] test nightly

--- src/exchangeability/martingales.rs
+++ src/exchangeability/martingales.rs
@@ -1,1 +1,8 @@
-//! 
+//! Martingales for exchangeability testing.
+use std::f64;
+use quadrature::integrate;
+use statrs::statistics::Variance;
+/// Exchangeability Martingale.
+///
+/// A generic exchangeability martingale, as described for example
+/// in

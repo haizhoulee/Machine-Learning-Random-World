83ad3a7246efff22a44b6bb99fb82f93fc967122
haizhoulee <134524345+haizhoulee@users.noreply.github.com>
2023-04-10 21:19:20 +0000

    Update partners.md

--- src/exchangeability/mod.rs
+++ src/exchangeability/mod.rs
@@ -1,1 +1,11 @@
-//! Defines Exchang
+//! Defines Exchangeability Martingales.
+//!
+//! Exchangeability martingales are tools for testing the
+//! exchangeability (and i.i.d.) assumption.
+//!
+//! # Examples
+//!
+//! ```
+//! extern crate rand;
+//! extern crate statrs;
+//! #[macro_use(array)

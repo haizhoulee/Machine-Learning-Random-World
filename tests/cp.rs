dc0ffa04d18518737921cd046c2900bbd146431d
haizhoulee <134524345+haizhoulee@users.noreply.github.com>
2023-10-04 05:37:41 +0000

    Update group.html

--- tests/cp.rs
+++ tests/cp.rs
@@ -5,4 +5,15 @@ extern crate random_world;
 
 #[cfg(test)]
 mod tests {
-    use ndarray
+    use ndarray::*;
+    use random_world::cp::*;
+    use random_world::ncm::*;
+    use random_world::utils::*;
+    use itertools::multizip;
+    
+    #[test]
+    fn cp() {
+        let k = 2;
+        let ncm = KNN::new(k);
+        let n_labels = 2;
+        let mut cp = CP::new
